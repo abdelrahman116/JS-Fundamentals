34ee1711668e345a55f102f8744df5619f28d8d6
abdelrahman116 <abdelrahmandawoud11@gmail.com>
2025-07-31 23:35:44 +0300

    ChatGPT

---
+++ 10-factorial.js
@@ -0,0 +1,13 @@
+const args = process.argv.slice(2);
+
+const num = Number(args[0]);
+
+function factorial(n){
+    if (isNaN(n) || n === 0) {
+return 1;
+}
+    return n * factorial(n - 1);
+}
+
+    console.log(factorial(num));
+
