c9e57eac91200ebf9df8ed19e82fb477200a69f7
abdelrahman116 <abdelrahmandawoud11@gmail.com>
2025-07-31 23:09:42 +0300

    try_2

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -2,6 +2,6 @@ const args = process.argv.slice(2);
 
 const result= ["C is fun","Python is cool","JavaScript is amazing"];
 
-for(let i = 0 ; i < result.length ; i++){
+for (let i = 0 ; i < result.length ; i++){
     console.log(`${result[i]}`);
 }
