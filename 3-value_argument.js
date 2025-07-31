595adf9d7084b42b515810ad82b57ea8bd5e7c79
abdelrahman116 <abdelrahmandawoud11@gmail.com>
2025-07-31 22:53:49 +0300

    try_4

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,7 +1,7 @@
 // checkArg.js
 const args = process.argv.slice(2); // ignore node + script path
 
-if (args[0] = undefined) {
+if (args[0] = undefined || args.slice(2) == "") {
   console.log("No argument");
 } else {
   console.log(args[0]);
